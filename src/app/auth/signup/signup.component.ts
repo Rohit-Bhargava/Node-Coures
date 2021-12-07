82db074e31cb3393e5a5199c7fbcfad46bfe74ee
Rohit-Bhargava <Rohit_Bhargava_Freelance@outlook.com>
2021-12-07 09:42:42 +0530

    Auth-final

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,25 +1,24 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { AuthService } from '../auth.service';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
+import { AuthService } from "../auth.service";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
 })
-export class SignupComponent  {
- 
+export class SignupComponent {
+  isLoading = false;
 
-  constructor(public authService: AuthService){}
+  constructor(public authService: AuthService) {}
 
-  onSignUp(form: NgForm){
-    if(form.invalid){
+  onSignup(form: NgForm) {
+    if (form.invalid) {
       return;
     }
-    this.authService.createUser(form.value.email, form.value.password)
+    this.isLoading = true;
+    this.authService.createUser(form.value.email, form.value.password);
   }
-
 }
 
 
